39979a20922a033f45615f3c491e6582448f8c78
ryo11aori-ship-it <ryo11aori@gmail.com>
2026-03-31 22:43:04 +0900

    bf_to_spaces.py を更新

--- tools/bf_to_spaces.py
+++ tools/bf_to_spaces.py
@@ -1,35 +1,20 @@
-#!/usr/bin/env python3
 import sys
-
 def main():
-    if len(sys.argv) < 2:
-        sys.stderr.write("Usage: python3 bf_to_spaces.py <input.bf>\n")
-        sys.exit(1)
-    try:
-        f = open(sys.argv[1], "r")
-        code = f.read()
-        f.close()
-    except Exception:
-        sys.exit(1)
-    op_map = {'>': 0, '<': 1, '+': 2, '-': 3, '.': 4, ',': 5, '[': 6, ']': 7}
-    for c in code:
-        if c in op_map:
-            v = op_map[c]
-            b2 = (v >> 2) & 1
-            b1 = (v >> 1) & 1
-            b0 = v & 1
-            if b2 == 0:
-                sys.stdout.write(" ")
-            else:
-                sys.stdout.write("\u3000")
-            if b1 == 0:
-                sys.stdout.write(" ")
-            else:
-                sys.stdout.write("\u3000")
-            if b0 == 0:
-                sys.stdout.write(" ")
-            else:
-                sys.stdout.write("\u3000")
-
-if __name__ == "__main__":
-    main()
+ if len(sys.argv)<2:
+  sys.exit(1)
+ try:
+  f=open(sys.argv[1],"r")
+  c=f.read()
+  f.close()
+ except:
+  sys.exit(1)
+ m={'>':0,'<':1,'+':2,'-':3,'.':4,',':5,'[':6,']':7,'*':8,'&':9,'i':10,'o':11,'!':12}
+ for x in c:
+  if x in m:
+   v=m[x]
+   sys.stdout.write("\u3000" if (v>>3)&1 else " ")
+   sys.stdout.write("\u3000" if (v>>2)&1 else " ")
+   sys.stdout.write("\u3000" if (v>>1)&1 else " ")
+   sys.stdout.write("\u3000" if v&1 else " ")
+if __name__=="__main__":
+ main()
